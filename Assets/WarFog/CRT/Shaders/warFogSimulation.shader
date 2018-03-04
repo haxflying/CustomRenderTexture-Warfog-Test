916151da09733dd8569af6403f688db917a49161
DESKTOP-DIKVTQF\91277 <912778422@qq.com>
2018-03-04 23:43:28 +0800

    Base Implementation

---
+++ Assets/WarFog/CRT/Shaders/warFogSimulation.shader
@@ -0,0 +1,43 @@
+﻿Shader "Unlit/warFogSimulation"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	CGINCLUDE
+	#include "UnityCustomRenderTexture.cginc"
+	sampler2D _MainTex;
+	float4 frag(v2f_customrendertexture i) : COLOR
+	{
+		//需要开启double buffer来读取上一次结果，但process后对上次的zone区域也有影响，原因不明
+
+		//fixed4 self = tex2D(_SelfTexture2D, i.globalTexcoord.xy);
+		fixed4 decal = tex2D(_MainTex, i.localTexcoord.xy);
+		return decal;// * decal.a；// + self * (1 - decal.a);
+		//return tex2D(_MainTex, i.globalTexcoord.xy);
+		//return float4(1,1,0,0);
+	}
+
+	float4 frag_update(v2f_customrendertexture i) : COLOR
+	{
+		return tex2D(_SelfTexture2D, i.globalTexcoord.xy);
+	}
+
+	ENDCG
+
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			//crt 可以直接使用传统的blend 来进行混合
+			Blend SrcAlpha OneMinusSrcAlpha 
+			CGPROGRAM
+			#pragma vertex CustomRenderTextureVertexShader
+			#pragma fragment frag			
+			ENDCG
+		}
+	}
+}
---
+++ Assets/WarFog/Scripts/warFogSimulation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warFogSimulation : MonoBehaviour {
+
+    public CustomRenderTexture texture;
+	void Start () {
+        texture.Initialize();
+	}
+	
+	
+	void Update () {
+        UpdateZone();
+	}
+
+    void UpdateZone()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Camera.main != null)
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                {
+                    CustomRenderTextureUpdateZone clearZone = new CustomRenderTextureUpdateZone();
+                    clearZone.needSwap = false;
+                    clearZone.passIndex = 0;
+                    clearZone.rotation = 0;
+                    clearZone.updateZoneCenter = new Vector2(hit.textureCoord.x, 1 - hit.textureCoord.y);
+                    clearZone.updateZoneSize = Vector2.one * 0.2f;
+                    texture.SetUpdateZones(new CustomRenderTextureUpdateZone[] { clearZone });
+                    texture.Update();
+                    print("new zone");
+                }
+            }
+        }
+    }
+}
